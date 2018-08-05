3a2aa0e56492d930d17a1c877489590cd3ec49c5
LILIT <lilit555ai@gmail.com>
2018-08-05 13:23:48 -0600

    Create driver_3.py

---
+++ driver_3.py
@@ -0,0 +1,13 @@
+import sys
+import numpy as np
+
+assignments = np.genfromtxt(sys.argv[1], delimiter = 1)
+
+
+def ac3(board):
+    return str(solved) + " AC3"
+
+
+
+def bts(board):
+    return str(solved) + " BTS"
