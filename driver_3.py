9f3ded5d147e6140093f066bfe4f0195de336cb3
LILIT <lilit555ai@gmail.com>
2018-08-07 22:12:39 -0600

    Add SBoard class

--- driver_3.py
+++ driver_3.py
@@ -1,13 +1,37 @@
 import sys
 import numpy as np
 
-assignments = np.genfromtxt(sys.argv[1], delimiter = 1)
+class SBoard(object):
+    def __init__(self, rows, columns, boxes, parent = None):
+        config = ''
+        domains = [1,2,3,4,5,6,7,8,9]
+        values = {}
+        for r in range(9):
+            for c in range(9):
+                config += str(int(rows[r][c]))
+                if rows[r][c] == 0.:
+                    if r < 3:
+                        b = int(r/3) + int(c/3)
+                    elif r < 6:
+                        b = int(r/3) + int(c/3) + 2
+                    elif r < 9:
+                        b = int(r/3) + int(c/3) + 4
+                    domain = set(domains) - set(rows[r]) - columns[c] - boxes[b]
+                    values[str(r) + str(c) + str(b)] = domain
+                    
+        self.rows = rows
+        self.columns = columns
+        self.boxes = boxes
+        self.values = values
+        self.domains = domains
+        self.config = config
+        if parent == None:
+            self.parent = self
+        else:
+            self.parent = parent
+        self.children = []
 
 
-def ac3(board):
-    return str(solved) + " AC3"
 
 
 
-def bts(board):
-    return str(solved) + " BTS"
